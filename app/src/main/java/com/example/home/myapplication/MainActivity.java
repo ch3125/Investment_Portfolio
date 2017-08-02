ba902aa495d2ffa29591d2f39a5ac7537b54c497
Vivek2503 <view.kedia@gmail.com>
2017-08-02 22:52:46 +0530

    Presses enter

--- app/src/main/java/com/example/home/myapplication/MainActivity.java
+++ app/src/main/java/com/example/home/myapplication/MainActivity.java
@@ -1,15 +1,12 @@
 package com.example.home.myapplication;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.text.TextUtils;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText inputEmail, inputPassword;
@@ -23,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
         // set the view now
         setContentView(R.layout.activity_main);
+
+
 
       //  Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
      //   setSupportActionBar(toolbar);
