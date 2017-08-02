f684ca797f6763522e9f24e1b0319a1e4696c6b4
Vivek2503 <view.kedia@gmail.com>
2017-08-02 23:11:59 +0530

    Presses enter

--- app/src/main/java/com/example/home/myapplication/MainActivity.java
+++ app/src/main/java/com/example/home/myapplication/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
         // set the view now
         setContentView(R.layout.activity_main);
-
+        //vivek
         
       //  Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
      //   setSupportActionBar(toolbar);
