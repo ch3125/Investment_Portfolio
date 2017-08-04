dae4441d6cf4a5e54e85226fcc3305834088dc0a
Vivek2503 <view.kedia@gmail.com>
2017-08-04 22:46:04 +0530

    Added the Choice class

--- app/src/main/java/com/example/home/myapplication/MainActivity.java
+++ app/src/main/java/com/example/home/myapplication/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Questionarre.class));
+                startActivity(new Intent(MainActivity.this, choice.class));
             }
         });
 
