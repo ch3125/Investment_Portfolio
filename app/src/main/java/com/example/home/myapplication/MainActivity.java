3509196fd629e35bf65aaf92e749341fa7268619
ch3125 <chhavi.alok25@gmail.com>
2017-08-06 10:13:41 +0530

    connections

--- app/src/main/java/com/example/home/myapplication/MainActivity.java
+++ app/src/main/java/com/example/home/myapplication/MainActivity.java
@@ -2,16 +2,27 @@ package com.example.home.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.example.home.myapplication.DataBase.DatabaseHelper;
+
+import java.text.ChoiceFormat;
 
 public class MainActivity extends AppCompatActivity {
     private EditText inputEmail, inputPassword;
     private ProgressBar progressBar;
     private Button btnSignup, btnLogin, btnReset;
+    private DatabaseHelper databaseHelper;
+    CoordinatorLayout cd;
+    private final AppCompatActivity activity = MainActivity.this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
         btnSignup = (Button) findViewById(R.id.btn_signup);
         btnLogin = (Button) findViewById(R.id.btn_login);
         btnReset = (Button) findViewById(R.id.btn_reset_password);
+        databaseHelper=new DatabaseHelper(activity);
+        cd=(CoordinatorLayout)findViewById(R.id.cdlayout);
 
         //Get Firebase auth instance
 
@@ -40,16 +53,11 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, SignupActivity.class));
             }
         });
+
+
+
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, choice.class));
-            }
-        });
-
-
-
-      /*  btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = inputEmail.getText().toString();
@@ -66,11 +74,29 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 progressBar.setVisibility(View.VISIBLE);
-
+                verifyFromSQLite();
                 //authenticate user
 
             }
-        });*/
+        });
+    }
+    private void verifyFromSQLite() {
+
+
+        if (databaseHelper.checkUser(inputEmail.getText().toString().trim()
+                , inputPassword.getText().toString().trim())) {
+
+
+            Intent accountsIntent = new Intent(activity, choice.class);
+            accountsIntent.putExtra("EMAIL", inputEmail.getText().toString().trim());
+         //   emptyInputEditText();
+            startActivity(accountsIntent);
+
+
+        } else {
+            // Snack Bar to show success message that record is wrong
+            Snackbar.make(cd, getString(R.string.error_valid_email_password), Snackbar.LENGTH_LONG).show();
+        }
     }
 
 }
